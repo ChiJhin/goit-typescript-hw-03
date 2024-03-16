0f9ad7bbef146138919f2714b44d6dc24798fe75
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-03-16 02:04:30 +0200

    full update

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,49 @@
+
+class Human {
+  constructor(private key: Key) {}
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+class Key {
+  private signature: number = Math.random();
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenant: Human[] = [];
+  comeIn(human: Human): void {
+    if (this.door) {
+      this.tenant.push(human);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const human = new Human(key);
 
-house.openDoor(person.getKey());
+house.openDoor(human.getKey());
 
-house.comeIn(person);
-
+house.comeIn(human);
 
 export {};
